04c56bfebe86641d8bf35150931b15b6866ecac3
Quinn Morrison <96103133+sqmorrison@users.noreply.github.com>
2025-05-02 19:56:57 -0500

    built out a partial route for registration

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ const express = require('express')
 const cors = require('cors')
 const {v4:uuidv4} = require('uuid')
 const sqlite3 = require('sqlite3').verbose()
+const datNow = new Date();
 
-
+//install bcrypt
 const dbSource = "review.db"
 const HTTP_PORT = 8000
 const db = new sqlite3.Database(dbSource)
@@ -15,6 +16,44 @@ const corsOptions = {
 };
 app.use(cors(corsOptions))
 app.use(express.json())
+
+/* 
+UserID INTEGER PRIMARY KEY AUTOINCREMENT,
+    FirstName TEXT NOT NULL,
+    LastName TEXT NOT NULL,
+    Email TEXT UNIQUE NOT NULL,
+    Password TEXT NOT NULL,
+    CreationDateTime DATETIME DEFAULT CURRENT_TIMESTAMP,
+    LastLoginDateTime DATETIME
+*/
+
+app.post('/studentRegister', (req, res, next) => {
+    const { strFullName, strEmail, strPassword, strConfirmPass, strClassCode } = req.body;
+
+    if (strPassword != strConfirmPass) {
+        res.status(401).json({message:"Passwords Must Match"})
+    }
+
+    const arrName = strFullName.split(" ");
+
+    const strFirstName = arrName[0];
+    const strLastName = arrName[1];
+
+    if (!strClassCode) {
+        const strCommand = 'INSERT INTO tblUsers (FirstName, LastName, Email, Password, CreationDateTime) VALUES (?,?,?,?,?)';
+    }
+    else {
+        const strCommand = 'INSERT INTO tblUsers (username, email, password) VALUES (?,?,?)';
+    }
+})
+
+app.post('/login', (req, res, next) => {
+    
+})
+
+app.get('/logout', (req, res) => {
+
+})
 
 app.get('/teams', (req, res, next) => {
     const strEmail = req.query.email.trim().toLowerCase();
