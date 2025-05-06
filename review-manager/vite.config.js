72fb77e57605093f399ad89d5298e42d6a0185d7
dwbyerly <dbyerly02@icloud.com>
2025-05-05 21:15:23 -0500

    registration mostly done backend-wise

--- review-manager/vite.config.js
+++ review-manager/vite.config.js
@@ -5,7 +5,11 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/studentRegister': 'http://localhost:8000'
+      '/studentRegister': 'http://localhost:8000',
+      '/login': 'http://localhost:8000/',
+      '/mfa': 'http://localhost:8000/',
+      '/instructorRegister': 'http://localhost:8000',
+      '/instructorCourseRegister': 'http://localhost:8000'
     }
   },
   build: {
